d9d66906f02ff359cdc816635b1386262428ab86
danili <daniel.li@appdynamics.com>
2017-12-09 14:22:34 -0800

    add Reverse Words in String III

---
+++ string/easy/reversewordinastringiii/README.md
@@ -0,0 +1,10 @@
+Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+**Example 1:**
+```
+_Input: "Let's take LeetCode contest"_
+_Output: "s'teL ekat edoCteeL tsetnoc"_
+```
+**Note:** In the string, each word is separated by single space and there will not be any extra space in the string.
+
+[LeetCode link](https://leetcode.com/problems/reverse-words-in-a-string-iii/description/)
---
+++ string/easy/reversewordinastringiii/Solution1.java
@@ -0,0 +1,19 @@
+package string.easy.reversewordsinastringiii;
+
+public class Solution1 {
+    public String reverseWords(String s) {
+        String words[] = s.split(" ");
+        StringBuilder res = new StringBuilder();
+        for (String w : words) {
+            res.append(new StringBuffer(w).reverse().toString() + " "); 
+        } 
+        return res.toString().trim();
+   }
+
+   public static void main(String[] args) {
+        String test = "this is a holiday's test";
+        Solution1 s = new Solution1();
+        System.out.println(test);
+        System.out.println(s.reverseWords(test));
+   }
+}
